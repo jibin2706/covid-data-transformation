45959482c111a8cf9f67620e4549fc7cabd4ae4b
Jibin Thomas <jibin.thomas2706@gmail.com>
2020-03-28 17:08:29 +0530

    Updated script to use the new file name

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ import pandas as pd
 
 # cleaning up data
 # removing unwanted columns and adding the sum of the incidents happened in all the regions in a particular day
-temp = pd.read_csv('./data/time_series_19-covid-Confirmed.csv')
-confirmed_total = pd.DataFrame(temp.iloc[:, 5:].sum(), columns=["confirmed"])
+temp = pd.read_csv('./data/time_series_covid19_confirmed_global.csv')
+confirmed_total = pd.DataFrame(temp.iloc[:, 5:].sum(), columns=[
+                               "confirmed"], dtype=int)
 
-temp = pd.read_csv('./data/time_series_19-covid-Deaths.csv')
-death_total = pd.DataFrame(temp.iloc[:, 5:].sum(), columns=["deaths"])
+temp = pd.read_csv('./data/time_series_covid19_deaths_global.csv')
+death_total = pd.DataFrame(
+    temp.iloc[:, 5:].sum(), columns=["deaths"], dtype=int)
 
-temp = pd.read_csv('./data/time_series_19-covid-Recovered.csv')
-recovered_total = pd.DataFrame(temp.iloc[:, 5:].sum(), columns=["recovered"])
+temp = pd.read_csv('./data/time_series_covid19_recovered_global.csv')
+recovered_total = pd.DataFrame(temp.iloc[:, 5:].sum(), columns=[
+                               "recovered"], dtype=int)
 
-# joining the result of the 3 parameters according to date
+# # joining the result of the 3 parameters according to date
 result = pd.concat([confirmed_total, death_total, recovered_total],
                    join='inner', axis=1)
 
